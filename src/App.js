e333db6c760fdae96e6d4317f483849bdeef61d1
Sebastian Civarolo <scivarolo@gmail.com>
2018-11-21 22:54:24 -0600

    include stylesheet and base structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-
+import DataManager from './modules/Data'
 class App extends Component {
+
+  state = {}
+
+  componentDidMount() {
+    // get some data
+  }
+
   render() {
     return (
-      <div className="app-wrapper">
-        <h1>Daily Journal</h1>
-      </div>
+      <section className="app-wrapper">
+        <h1 className="site-title">Daily Journal</h1>
+
+        <section className="grid-wrapper">
+          <div className="left-column">
+            <div className="sticky-wrapper">
+              <section className="form-wrapper"></section>
+              <section id="mood-filter" className="mood-filter"></section>
+            </div>
+          </div>
+          <section id="entries" className="entries-wrapper"></section>
+        </section>
+      </section>
     );
   }
 }
 
-export default App;
+export default App
